86185ada924a6120c73a0f4448ad7999db13d150
abkkm <areefbasha.tech@gmail.com>
2023-01-15 19:08:06 +0530

    01-spring

---
+++ springboot3-new1/01-spring/learn-spring-framework-02/src/main/java/com/example/learnspringframework/examples/a0/SimpleSpringContextLauncherApplication.java
@@ -0,0 +1,24 @@
+package com.example.learnspringframework.examples.a0;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class SimpleSpringContextLauncherApplication {
+	
+	public static void main(String[] args) {
+    System.out.println("*****************");
+		try (var context = 
+				new AnnotationConfigApplicationContext
+					(SimpleSpringContextLauncherApplication.class)) {
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+
+		}
+	}
+}
